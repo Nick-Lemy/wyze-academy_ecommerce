7f35084af180738e455a7e52ba1193d2ae41da29
Nick-Lemy <n.kayiranga@alustudent.com>
2025-09-27 14:15:42 +0200

    feat(frontend): update Hero component button size and spacing

--- frontend/src/app/_components/Hero.tsx
+++ frontend/src/app/_components/Hero.tsx
@@ -6,11 +6,11 @@ import HeroImage from "@/assets/hero-image.png";
 const Hero = () => {
   return (
     <div className=" bg-secondary rounded-2xl flex items-center justify-start px-10 my-5">
-      <div className="space-y-6 flex-1">
+      <div className="space-y-8 flex-1">
         <h1 className="text-7xl text-primary font-semibold">
           Grab Upto 50% Off On Selected Items on the store
         </h1>
-        <Button variant="default" size="xs" className="mt-4">
+        <Button variant="default" size="sm">
           Shop Now
         </Button>
       </div>

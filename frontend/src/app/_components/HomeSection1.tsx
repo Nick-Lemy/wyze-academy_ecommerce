a72e9e33dec786e72c87356ca2b2030daa9ca8e3
Nick-Lemy <n.kayiranga@alustudent.com>
2025-09-27 15:49:39 +0200

    feat(frontend): add SearchProducts component for enhanced product filtering

--- frontend/src/app/_components/HomeSection1.tsx
+++ frontend/src/app/_components/HomeSection1.tsx
@@ -1,17 +1,19 @@
 import ProductCard from "@/components/ProductCard";
 import React from "react";
 import { sampleProducts } from "@/lib/sampleData";
+import SearchProducts from "./SearchProducts";
 
 const HomeSection1 = () => {
   return (
-    <div>
+    <div className="space-y-7">
+      <div>
+        <SearchProducts />
+      </div>
       <h1 className="text-3xl font-semibold">Headphones For You!</h1>
       <div className="flex flex-wrap gap-6 mt-6">
-        {/* Product List Here */}
         {sampleProducts.map((product) => (
           <ProductCard key={product.id} {...product} />
         ))}
-        {/* Example ProductCard */}
       </div>
     </div>
   );
---
+++ frontend/src/app/_components/SearchProducts.tsx
@@ -0,0 +1,42 @@
+import { SearchIcon } from "lucide-react";
+import React from "react";
+
+const SearchProducts = () => {
+  return (
+    <div className="relative flex items-center justify-between">
+      <div className="flex gap-4">
+        <div className=" border-primary/50 border rounded-xl px-3 bg-primary-hover">
+          <select className="p-2 text-white">
+            <option value="all">All Categories</option>
+            <option value="headphones">Headphones</option>
+            <option value="smartphones">Smartphones</option>
+            <option value="laptops">Laptops</option>
+            <option value="watches">Watches</option>
+          </select>
+        </div>
+        <div className=" border-primary/50 border rounded-xl px-3 bg-primary-hover">
+          <select className="p-2 text-white">
+            <option value="all">All Prices</option>
+            <option value="0-50">$0 - $50</option>
+            <option value="51-200">$51 - $200</option>
+            <option value="201-500">$201 - $500</option>
+            <option value="501-1000">$501 - $1000</option>
+            <option value="1000+">$1000+</option>
+          </select>
+        </div>
+      </div>
+      <div className="w-1/4">
+        <div className="relative flex flex-1 text-[16px] items-center justify-end">
+          <input
+            className="border focus:ring-2 focus-visible::ring-primary w-full pl-10 border-primary rounded-xl p-2"
+            type="text"
+            placeholder="Search products..."
+          />
+          <SearchIcon className="absolute left-0 inline-block ml-2 text-primary" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchProducts;

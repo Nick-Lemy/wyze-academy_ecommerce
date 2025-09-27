b32f12286077e36062b31b530bb039e9112f4228
Nick-Lemy <n.kayiranga@alustudent.com>
2025-09-27 13:36:26 +0200

    feat(frontend): update NavBar component to use Link for navigation

--- frontend/src/app/_components/NavBar.tsx
+++ frontend/src/app/_components/NavBar.tsx
@@ -1,33 +1,55 @@
 import React from "react";
 import Logo from "@/assets/logo.jpg";
 import Image from "next/image";
+import { ShoppingCart, UserIcon } from "lucide-react";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <nav className="flex items-center justify-between p-4 bg-white">
+    <nav className="flex items-center justify-between bg-white">
       <div className="flex gap-1 items-center">
         <Image src={Logo} alt="Logo" className="w-12 mr-2" />
         <span className="text-lg text-primary font-bold">Nick&apos;s Shop</span>
       </div>
       <div className="space-x-4">
-        <a
-          href="#"
+        <Link
+          href="/"
           className="text-primary/70 hover:text-primary font-semibold"
         >
           Home
-        </a>
-        <a
-          href="#"
+        </Link>
+        <Link
+          href="/whats-new"
           className="text-primary/70 hover:text-primary font-semibold"
         >
-          Shop
-        </a>
-        <a
-          href="#"
+          What&apos;s New
+        </Link>
+        <Link
+          href="/contact"
+          className="text-primary/70 hover:text-primary font-semibold"
+        >
+          Contact
+        </Link>
+        <Link
+          href="/about"
           className="text-primary/70 hover:text-primary font-semibold"
         >
           About
-        </a>
+        </Link>
+      </div>
+      <div className="flex  gap-5">
+        <Link
+          href="/cart"
+          className="text-primary/70 hover:text-primary font-semibold"
+        >
+          <ShoppingCart className="text-primary size-7" />
+        </Link>
+        <Link
+          href="/account"
+          className="text-primary/70 hover:text-primary font-semibold"
+        >
+          <UserIcon className="size-7 text-primary" />
+        </Link>
       </div>
     </nav>
   );

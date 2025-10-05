2c0461250a143369907afccc49d139dd974d2d50
Nick-Lemy <n.kayiranga@alustudent.com>
2025-10-06 01:39:08 +0200

    feat: remove Footer component and adjust layout padding

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "../components/NavBar";
-import Footer from "../components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white font-sans pt-5 text-black`}
       >
-        <header className=" px-20">
+        <header className=" px-10">
           <NavBar />
         </header>
-        <div className=" px-20">{children}</div>
-        <Footer />
+        <div className=" px-10">{children}</div>
       </body>
     </html>
   );

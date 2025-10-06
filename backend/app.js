0e53e624fb6aa6b1b224223dd2d7a17e987e7c90
Nick-Lemy <n.kayiranga@alustudent.com>
2025-10-06 03:23:01 +0200

    feat(backend): integrate Cloudinary for file uploads and add new product endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,34 @@
 dotenv.config();
 import express from 'express'
 import dotenv from 'dotenv'
-import { MONGODB_URL, PORT } from './configs/variables.js';
+import { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME, PORT } from './configs/variables.js';
 import Product from './models/product.js';
 import './configs/database.js';
+import cloudinary from 'cloudinary';
+import fileUpload from 'express-fileupload';
+
+cloudinary.v2.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
+})
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.post('/add-file', async (req, res) => {
+
+})
+
 
 app.get('/products', async (req, res) => {
     try {
@@ -23,12 +41,19 @@ app.get('/products', async (req, res) => {
 });
 
 app.post('/products', async (req, res) => {
+    const { file } = req.files;
+    const { title, price, description, miniTitle, category, features, sku } = req.body;
+    console.log({ title, price, description, miniTitle, category, features, sku })
     try {
-        const product = new Product(req.body);
+        const result = await cloudinary.uploader.upload(file.tempFilePath, {
+            folder: "users",
+        })
+        const { secure_url } = result
+        const product = new Product({ title, price, description, miniTitle, category, features, sku, image: secure_url });
         await product.save();
-        res.status(201).send(product);
+        res.status(201).send({ ...product });
     } catch (error) {
-        res.status(400).send({ error: 'Bad Request' });
+        res.status(400).send({ error: `Error: ${error}` });
     }
 });
 
--- backend/configs/variables.js
+++ backend/configs/variables.js
@@ -4,3 +4,6 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const MONGODB_URL = process.env.MONGODB_URL
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET

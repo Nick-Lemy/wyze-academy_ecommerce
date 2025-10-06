1fec47760b6aeaf60e8904adbe70c61fe2c76316
Nick-Lemy <n.kayiranga@alustudent.com>
2025-10-06 02:53:27 +0200

    feat(backend): implement product model and CRUD endpoints for products

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,35 @@
+dotenv.config();
 import express from 'express'
 import dotenv from 'dotenv'
-import { PORT } from './configs/varaibles';
-dotenv.config();
+import { MONGODB_URL, PORT } from './configs/variables.js';
+import Product from './models/product.js';
+import './configs/database.js';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find();
+        console.log(products)
+        res.send(products);
+    } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
+});
+
+app.post('/products', async (req, res) => {
+    try {
+        const product = new Product(req.body);
+        await product.save();
+        res.status(201).send(product);
+    } catch (error) {
+        res.status(400).send({ error: 'Bad Request' });
+    }
 });
 
 app.listen(PORT, () => {
